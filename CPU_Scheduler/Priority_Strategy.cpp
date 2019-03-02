cc2ea8e264b51368f099ce360a71bc877490a69e
Windows X <windowsx@windowsde-macbook-pro.local>
2019-03-02 01:56:15 -0800

    add priority strategy

---
+++ CPU_Scheduler/Priority_Strategy.cpp
@@ -0,0 +1,29 @@
+#include "Priority_Strategy.h"
+
+Priority_Strategy::Priority_Strategy(std::shared_ptr<Context> c) : ScheduleStrategy(c) {
+    //nothing needed here for SJF
+}
+
+void Priority_Strategy::run() {
+    //nothing needed here for SJF
+}
+
+void Priority_Strategy::schedule() {
+    std::shared_ptr<Thread> threadToSchedule;
+    for (std::list<std::shared_ptr<Thread>>::iterator it = context->ReadyList->begin(); it != context->ReadyList->end(); ++it){
+        if (comparePriority(*it, threadToSchedule)){
+            threadToSchedule = *it;
+        }
+    }
+    context->ReadyList->remove(threadToSchedule);
+    std::shared_ptr<Thread> lastThread = context->scheduler->preempt(threadToSchedule); //move scheduled thread to CPU and save the last thread
+    context->scheduler->finishThread(lastThread);
+}
+
+void Priority_Strategy::addThread() {
+    //nothing needed here for SJF
+}
+
+bool Priority_Strategy::comparePriority(std::shared_ptr<Thread> threadA, std::shared_ptr<Thread> threadB){
+    return threadA->priority < threadB->priority;
+}

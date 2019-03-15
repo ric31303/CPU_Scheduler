5ecb27ce3209a2f0340bd015945c63a8d417fdca
james <james@jibaoviewer.com>
2019-03-14 23:57:14 -0700

    [Modify] Update .gitignore

---
+++ commands/dev.sh
@@ -0,0 +1,22 @@
+#IO_100
+. ../config/dev.config
+echo "$_resultPath"
+echo "$_testPath"
+echo "$_strategyAmount"
+echo "$_testAmount"
+
+strategy=0
+
+fileName="t5"
+Folder=""
+testFileType=".txt";
+
+resultPath="$_resultPath/$Folder"
+testPath="$_testPath/$Folder"
+
+echo "$testPath" "$resultPath"
+# ../config/dev.config
+# sudo echo "resultPath"
+
+../build/CPU_Scheduling "$strategy" "$fileName" "$testPath" "$resultPath"
+
